e1e566a18d344765180354712301d73f01c39a15
Eran Mukamel <emukamel@ucsd.edu>
2019-02-26 17:12:57 -0800

    Hide heatmap normalization unless >1 gene

--- scmdb_py/assets/scripts/customview.js
+++ scmdb_py/assets/scripts/customview.js
@@ -592,6 +592,7 @@ function updateGeneElements(updateMCHScatter=true) {
         $('#epiBrowserLink').removeClass('disabled');
         if($("#geneName").select2('data').length > 1) {
             $('#normalize-heatmap').show();
+            $('#methylation-box-heat-normalize-toggle-div').show();
             $('#methylation-box-heat-normalize-toggle').prop('disabled', false);
             updateMethylationHeatmap();
             updateCorrelatingGeneDataTable("");
@@ -605,6 +606,7 @@ function updateGeneElements(updateMCHScatter=true) {
         }
         else{
             $('#normalize-heatmap').hide();
+            $('#methylation-box-heat-normalize-toggle-div').hide();
             $('#methylation-box-heat-normalize-toggle').prop('disabled', true);
             //updateOrthologToggle();
             updateMCHBoxPlot();
