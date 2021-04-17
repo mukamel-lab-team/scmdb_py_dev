a363052fae919b3e450a3951019d3d6207fec4e1
Eran Mukamel <emukamel@ucsd.edu>
2021-04-17 16:31:05 -0700

    In tabular_ensemble cut off long table entries

--- scmdb_py/assets/scripts/tabular_ensemble.js
+++ scmdb_py/assets/scripts/tabular_ensemble.js
@@ -1,10 +1,10 @@
 function initEnsembleDataTable() {
-    
+
     const numberWithCommas = (x) => {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-    
-    var table = $('#ensemble-table').DataTable({
+  }
+
+  var table = $('#ensemble-table').DataTable({
         "order": [[3, 'desc']], //Initially sort by "Total Cells (snmC-seq)" in descending order. 
         "pageLength": 50,
         "ajax": {
@@ -12,46 +12,69 @@ function initEnsembleDataTable() {
             "dataSrc": ""
         },
         "columns": [
-            {
-                "data": null,
-                "className": 'details-control dt-center',
-                "orderable": false,
-                "defaultContent": '<i class="fas fa-plus-circle"></i>'
-            },
-            {"data": "ensemble_id"},
-            {"data": "ensemble_name"},
-            {"data": "total_methylation_cells", "className": 'dt-center'},
-            {"data": "total_snATAC_cells", "className": 'dt-center'},
-            {"data": "ABA_regions_acronym", "className": 'dt-center'},
-            {"data": "slices", "className": 'dt-center'},
-            {"data": "num_datasets", "className": 'dt-center'},
-            {"data": "target_regions_rs2_acronym", "className": 'dt-center'},
-            {
-                "data": "public_access_icon",
-                "className": 'dt-center',
-                "fnCreatedCell": function(nTd, sData, oData, iRow, iCol) {
-                    $(nTd).html('<i class="'+oData.public_access_icon+'" style="color:'+oData.public_access_color+';"></i>');
+        {
+            "data": null,
+            "className": 'details-control dt-center',
+            "orderable": false,
+            "defaultContent": '<i class="fas fa-plus-circle"></i>'
+        },
+        {"data": "ensemble_id"},
+        {
+            "data": "ensemble_name",
+            "render": function ( data, type, row, meta ) {
+                return type === 'display' && data.length > 20 ? 
+                '<span title="'+data+'">'+data.substr( 0, 38 )+'...</span>' : 
+                data;
+            }
+ 
+        },
+        {"data": "total_methylation_cells", "className": 'dt-center'},
+        {"data": "total_snATAC_cells", "className": 'dt-center'},
+        {
+            "data": "ABA_regions_acronym", 
+            "className": 'dt-center', 
+            "render": function ( data, type, row, meta ) {
+                return type === 'display' && data.length > 20 ? 
+                '<span title="'+data+'">'+data.substr( 0, 38 )+'...</span>' : 
+                data;
+            }
+        },
+        {
+            "data": "slices", "className": 'dt-center',
+            "render": function ( data, type, row, meta ) {
+                return type === 'display' && data.length > 10 ? 
+                '<span title="'+data+'">'+data.substr( 0, 38 )+'...</span>' : 
+                data;
+            }
+        },
+        {"data": "num_datasets", "className": 'dt-center'},
+        {"data": "target_regions_rs2_acronym", "className": 'dt-center'},
+        {
+            "data": "public_access_icon",
+            "className": 'dt-center',
+            "fnCreatedCell": function(nTd, sData, oData, iRow, iCol) {
+                $(nTd).html('<i class="'+oData.public_access_icon+'" style="color:'+oData.public_access_color+';"></i>');
+            }
+        },
+        {
+            "data": "ensemble_id",
+            "className": 'redirect-control dt-center',
+            "orderable": false,
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html('<a href="'+$SCRIPT_ROOT+'/Ens'+oData.ensemble_id+'"><i class="fas fa-eye"></i></a>');
+            }
+        },
+        {
+            "data": "annoj_exists",
+            "className": 'dt-center',
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                if ( oData.annoj_exists == 1 )  {
+                    $(nTd).html('<a href="https://brainome.ucsd.edu/annoj_private/CEMBA/cemba.php?ens='+oData.ensemble_id+'" target="_blank"><i class="fas fa-dna"></i></a>');
+                } else {
+                    $(nTd).html('<i class="fas fa-dna" style="color: gray"></i>');
                 }
-            },
-            {
-                "data": "ensemble_id",
-                "className": 'redirect-control dt-center',
-                "orderable": false,
-                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                    $(nTd).html('<a href="'+$SCRIPT_ROOT+'/Ens'+oData.ensemble_id+'"><i class="fas fa-eye"></i></a>');
-                }
-            },
-            {
-                "data": "annoj_exists",
-                "className": 'dt-center',
-                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                    if ( oData.annoj_exists == 1 )  {
-                        $(nTd).html('<a href="https://brainome.ucsd.edu/annoj_private/CEMBA/cemba.php?ens='+oData.ensemble_id+'" target="_blank"><i class="fas fa-dna"></i></a>');
-                    } else {
-                        $(nTd).html('<i class="fas fa-dna" style="color: gray"></i>');
-                    }
-                }
-            },
+            }
+        },
         ],
         "fixedHeader": {
             "header": false,
@@ -62,52 +85,52 @@ function initEnsembleDataTable() {
 
             // Total mC cells
             grand_total_methylation_cells = api
-                .column( 3 )
-                .data()
-                .reduce( function (a, b) {
-                    return a+b;
-                }, 0 );
- 
+            .column( 3 )
+            .data()
+            .reduce( function (a, b) {
+                return a+b;
+            }, 0 );
+
             // Total snATAC cells
             grand_total_snATAC_cells = api
-                .column( 4 )
-                .data()
-                .reduce( function (a, b) {
-                    return a+b;
-                }, 0 );
- 
+            .column( 4 )
+            .data()
+            .reduce( function (a, b) {
+                return a+b;
+            }, 0 );
+
             grand_total_ensembles = api
-                .column( 1 )
-                .data().map(function(e){ return (e>0); })
-                .reduce( function (a, b) {
-                    return a+b;
-                }, 0 );
- 
+            .column( 1 )
+            .data().map(function(e){ return (e>0); })
+            .reduce( function (a, b) {
+                return a+b;
+            }, 0 );
+
             // Update footer
             $( api.column( 10 ).footer() ).html(
                 grand_total_ensembles+' ensembles including '+numberWithCommas(grand_total_methylation_cells) +' mC cells, '+ 
                 numberWithCommas(grand_total_snATAC_cells) +
                 ' ATAC cells across (NOTE some cells appear in multiple ensembles)'
-            );
+                );
         }
     });
-    
 
-    $('#ensemble-table tbody').on('click', 'td.details-control', function() {
-        var tr = $(this).closest('tr');
-        var row = table.row(tr);
 
-        if (row.child.isShown()) {
-            row.child.hide();
-            tr.removeClass('shown');
-            $(this).html('<i class="fas fa-plus-circle"></i>');
-        }
-        else {
-            row.child(format(row.data())).show();
-            tr.addClass('shown');
-            $(this).html('<i class="fas fa-minus-circle"></i>');
-        }
-    });
+  $('#ensemble-table tbody').on('click', 'td.details-control', function() {
+    var tr = $(this).closest('tr');
+    var row = table.row(tr);
+
+    if (row.child.isShown()) {
+        row.child.hide();
+        tr.removeClass('shown');
+        $(this).html('<i class="fas fa-plus-circle"></i>');
+    }
+    else {
+        row.child(format(row.data())).show();
+        tr.addClass('shown');
+        $(this).html('<i class="fas fa-minus-circle"></i>');
+    }
+});
 
 }
 
@@ -120,55 +143,55 @@ function format ( d ) {
     }
 
     var out = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-            '<tr>'+
-                '<td><b>Description:</b></td>'+
-                '<td>'+d.description+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td><b>Datasets in ensemble (snmC-seq):</b></td>'+
-                '<td>'+
-                    '<table>';
+    '<tr>'+
+    '<td><b>Description:</b></td>'+
+    '<td>'+d.description+'</td>'+
+    '</tr>'+
+    '<tr>'+
+    '<td><b>Datasets in ensemble (snmC-seq):</b></td>'+
+    '<td>'+
+    '<table>';
     if (d.datasets_rs1 != "") { out = out+'<tr><td><b>RS1</b></td><td>'+d.datasets_rs1+'</td></tr>' }
-    if (d.datasets_rs2 != "") { out = out+'<tr><td><b>RS2</b></td><td>'+d.datasets_rs2+'</td></tr>' }
-    out = out + 
-                    '</table>'+
-                '</td>'+
-            '</tr>'
-    if (d.snATAC_datasets_rs1+d.snATAC_datasets_rs2 != "") { out = out+
-            '<tr>'+
-                '<td><b>Datasets in ensemble (snATAC-seq):</b></td>'+
-                '<td>'+
-                    '<table>'}
-    if (d.snATAC_datasets_rs1 != "") { out = out+'<tr><td><b>RS1</b></td><td>'+d.snATAC_datasets_rs1+'</td></tr>' }
-    if (d.snATAC_datasets_rs2 != "") { out = out+'<tr><td><b>RS2</b></td><td>'+d.snATAC_datasets_rs2+'</td></tr>' }
-    if (d.snATAC_datasets_rs1+d.snATAC_datasets_rs2 != "") {
+        if (d.datasets_rs2 != "") { out = out+'<tr><td><b>RS2</b></td><td>'+d.datasets_rs2+'</td></tr>' }
             out = out + 
+        '</table>'+
+        '</td>'+
+        '</tr>'
+        if (d.snATAC_datasets_rs1+d.snATAC_datasets_rs2 != "") { out = out+
+            '<tr>'+
+            '<td><b>Datasets in ensemble (snATAC-seq):</b></td>'+
+            '<td>'+
+            '<table>'}
+            if (d.snATAC_datasets_rs1 != "") { out = out+'<tr><td><b>RS1</b></td><td>'+d.snATAC_datasets_rs1+'</td></tr>' }
+                if (d.snATAC_datasets_rs2 != "") { out = out+'<tr><td><b>RS2</b></td><td>'+d.snATAC_datasets_rs2+'</td></tr>' }
+                    if (d.snATAC_datasets_rs1+d.snATAC_datasets_rs2 != "") {
+                        out = out + 
                         '</table>'+
-                '</td>'+
-            '</tr>'}
-            out = out + 
-            '<tr>'+
-                '<td><b>Dissection region(s):</b></td>'+
-                '<td>'+d.ABA_regions_description+'</td>'+
-            '</tr>'
-    if (d.target_regions_rs2_descriptive != "") {
-        out = out+
-            '<tr>'+
-                '<td><b>Target region(s):</b></td>'+
-                '<td>'+d.target_regions_rs2_descriptive+'</td>'+
-            '</tr>'
-        }
+                        '</td>'+
+                        '</tr>'}
+                        out = out + 
+                        '<tr>'+
+                        '<td><b>Dissection region(s):</b></td>'+
+                        '<td>'+d.ABA_regions_description+'</td>'+
+                        '</tr>'
+                        if (d.target_regions_rs2_descriptive != "") {
+                            out = out+
+                            '<tr>'+
+                            '<td><b>Target region(s):</b></td>'+
+                            '<td>'+d.target_regions_rs2_descriptive+'</td>'+
+                            '</tr>'
+                        }
     var slices=[... new Set(d.slices.split(', ').map(x => x.charAt(0)))] // Get the unique slices contributing to this ensemble
     out = out + 
-        '</table>'+
-        '<div><b>Dissection drawings for all slices:</b><br>'
+    '</table>'+
+    '<div><b>Dissection drawings for all slices:</b><br>'
     var url=''
     for (i=0; i<slices.length; i++) {
         url='https://brainome.ucsd.edu/CEMBA_dissection_images/CEMBA_Slice'+slices[i]
         out = out+'<td> '+
-          '<a href="/tabular/ensemble?slideIndex='+slices[i]+'">'+
-          '<img src="'+url+'_sm.png" ' +
-          ' width=200 alt="CEMBA Slice'+slices[i]+'"></a> </td>'
+        '<a href="/tabular/ensemble?slideIndex='+slices[i]+'">'+
+        '<img src="'+url+'_sm.png" ' +
+        ' width=200 alt="CEMBA Slice'+slices[i]+'"></a> </td>'
     }
     out = out + '</div>'
     return out
